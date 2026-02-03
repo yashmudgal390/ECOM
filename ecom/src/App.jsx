17bceb892213072ff418cc89dc57cdb6ffbb6363
Yash Mudgal <yashmudgal390@gmail.com>
2026-02-03 21:52:00 +0530

    home style

--- ecom/src/App.jsx
+++ ecom/src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {Route,Routes} from 'react-router-dom';
 import './App.css'
+import { Home } from './pages/Home';
 
 function App() {
   
 
   return (
     <>
-     
+     <Routes>
+      <Route path='/' element={<Home/>}></Route>
+     </Routes>
     </>
   )
 }
