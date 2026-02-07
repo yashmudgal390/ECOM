5430c2072acf70037e02a854cc98a874ee5b1383
Yash Mudgal <yashmudgal390@gmail.com>
2026-02-07 23:23:30 +0530

    api

---
+++ ecom/src/api/getAllCategories.jsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+const baseUrl ='https://api.escuelajs.co/api/v1';
+export const getAllProducts=async ()=>{
+const url =`${baseUrl}/categories`;
+try{
+    const {data}=await axios.get(url)
+    
+    return data
+}catch(err){
+    return err
+}
+ }
