c5f5fbd92483eb801c5736463d335a03dcad52ce
Yash Mudgal <yashmudgal390@gmail.com>
2026-02-08 14:49:20 +0530

    added filter by category

--- ecom/src/api/getAllCategories.jsx
+++ ecom/src/api/getAllCategories.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 const baseUrl ='https://api.escuelajs.co/api/v1';
-export const getAllProducts=async ()=>{
+export const getAllCategories=async ()=>{
 const url =`${baseUrl}/categories`;
 try{
     const {data}=await axios.get(url)
